cfa3b22836d36fba5631e85f66e05c21503eb8c6
georgedouzas <georgios.douzas@gmail.com>
2019-06-21 20:44:54 +0300

    Prepare cli function

---
+++ tools/cli.py
@@ -0,0 +1,19 @@
+"""
+Implement the command-line interface.
+"""
+
+# Author: Georgios Douzas <gdouzas@icloud.com>
+# License: MIT
+
+from argparse import ArgumentParser
+from os.path import dirname, join, abspath
+from pickle import load
+
+import pandas as pd
+
+from . import DATA_PATH, EXPERIMENTS_PATH
+
+
+def run():
+    """Parse command-line arguments."""
+    pass
