e721710ec917669a7732a3a06fc7eac39057286e
georgedouzas <georgios.douzas@gmail.com>
2019-07-29 02:35:11 +0300

    Fixed functions input

--- tools/format.py
+++ tools/format.py
@@ -7,12 +7,16 @@ Format experimental results.
 
 import pandas as pd
 
-METRICS_NAMES_MAPPING = {'roc_auc': 'AUC', 'f1': 'F-SCORE', 'geometric_mean_score': 'G-MEAN'}
+METRICS_NAMES_MAPPING = {
+    'roc_auc': 'AUC', 
+    'f1': 'F-SCORE', 
+    'geometric_mean_score': 'G-MEAN', 
+    'accuracy': 'ACCURACY'
+}
 
 
-def generate_mean_std_tbl(experiment, name):
+def generate_mean_std_tbl(mean_vals, std_vals):
     """Generate table that combines mean and sem values."""
-    mean_vals, std_vals = getattr(experiment, f'mean_{name}_'), getattr(experiment, f'sem_{name}_')
     index = mean_vals.iloc[:, :2]
     scores = mean_vals.iloc[:, 2:].applymap('{:,.2f}'.format) + r" $\pm$ "  + std_vals.iloc[:, 2:].applymap('{:,.2f}'.format)
     tbl = pd.concat([index, scores], axis=1)
@@ -20,9 +24,8 @@ def generate_mean_std_tbl(experiment, name):
     return tbl
 
 
-def generate_pvalues_tbl(experiment, name):
+def generate_pvalues_tbl(tbl):
     """Format p-values."""
-    tbl = getattr(experiment, f'{name}_test_').copy()
     for name in tbl.dtypes[tbl.dtypes == float].index:
         tbl[name] = tbl[name].apply(lambda pvalue: '%.1e' % pvalue)
     tbl['Metric'] = tbl['Metric'].apply(lambda metric: METRICS_NAMES_MAPPING[metric])
