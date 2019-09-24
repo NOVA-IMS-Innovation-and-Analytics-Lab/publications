1a9f9d08d8e0b0b4b854848e83a0123e74cda8dd
georgedouzas <georgios.douzas@gmail.com>
2019-09-25 02:47:56 +0300

    Added research-learn dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         'requests>=2.0.0',
-        'xlrd >= 1.0.0'
+        'xlrd >= 1.0.0',
+        'research-learn == 0.1.0'
     ],
     entry_points={
         'console_scripts':['run=tools.cli:run']
