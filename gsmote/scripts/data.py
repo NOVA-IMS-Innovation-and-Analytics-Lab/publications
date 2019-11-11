255389f4c27bcc806e9e58773a4f070a65147879
georgedouzas <georgios.douzas@gmail.com>
2019-11-12 01:14:20 +0200

    Added data scripts

--- gsmote/scripts/data.py
+++ gsmote/scripts/data.py
@@ -5,6 +5,22 @@ Extract the database.
 # Author: Georgios Douzas <gdouzas@icloud.com>
 # License: MIT
 
-# TODO
-# 
-# Implement data script
+import sys
+from os.path import join, dirname
+from sqlite3 import connect
+
+sys.path.append(join(dirname(__file__), '..', '..'))
+from utils import ImbalancedBinaryDatasets
+
+DATA_PATH = join(dirname(__file__), '..', 'data', 'gsmote.db')
+
+
+if __name__ == '__main__':
+
+    # Download datasets
+    data = ImbalancedBinaryDatasets().download().datasets_
+
+    # Save data to database
+    with connect(DATA_PATH) as connection:
+        for name, df in data:
+            df.to_sql(name, connection, if_exists='replace')
