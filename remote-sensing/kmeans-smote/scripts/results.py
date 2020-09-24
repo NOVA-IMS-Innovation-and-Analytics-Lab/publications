4facbb5cdde1f2d80db7188cb07a577a78c630b7
joaopfonseca <jpmrfonseca@gmail.com>
2020-09-24 11:04:43 +0100

    Added 1st of kmeans smote paper (applied to lulc)

--- remote-sensing/kmeans-smote/scripts/results.py
+++ remote-sensing/kmeans-smote/scripts/results.py
@@ -9,6 +9,7 @@ Generate the main experimental results.
 import sys
 from os.path import join, dirname
 
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -17,8 +18,8 @@ from sklearn.dummy import DummyClassifier
 from sklearn.metrics import make_scorer, SCORERS
 from imblearn.over_sampling import (
     SMOTE,
-    RandomOverSampler, 
-    BorderlineSMOTE, 
+    RandomOverSampler,
+    BorderlineSMOTE,
     ADASYN
 )
 from clover.over_sampling import (
@@ -27,28 +28,30 @@ from clover.over_sampling import (
 )
 from rlearn.tools import ImbalancedExperiment
 
+sys.path.append(join(dirname(__file__), '..', '..', '..'))
 from utils import load_datasets, generate_paths
 
 
 CONFIG = {
     'oversamplers': [
-        ('NONE', None, {}),
-        ('ROS', RandomOverSampler(), {}),
-        ('SMOTE', ClusterOverSampler(SMOTE()), {'oversampler__k_neighbors': [3, 5]}),
-        ('B-SMOTE', ClusterOverSampler(BorderlineSMOTE()), {'oversampler__k_neighbors': [3, 5]}),
-        ('K-SMOTE', KMeansSMOTE(), {
-            'kmeans_estimator': [0.1, 0.3, 0.5, 0.7],
-            'distances_exponent': ['auto', 2, 5],
-            'imbalance_ratio_threshold': ['auto', 0.5, 1.0],
-            'k_neighbors': [3, 5]
-            }
-        ),
+       ('NONE', None, {}),
+       ('ROS', RandomOverSampler(), {}),
+       ('SMOTE', ClusterOverSampler(SMOTE()), {'oversampler__k_neighbors': [3, 5]}),
+       ('B-SMOTE', ClusterOverSampler(BorderlineSMOTE()), {'oversampler__k_neighbors': [3, 5]}),
+       ('K-SMOTE', KMeansSMOTE(), [
+           {'kmeans_estimator':[1], 'imbalance_ratio_threshold': [np.inf], 'k_neighbors':[3, 5]}, {
+           'kmeans_estimator': [.05, 0.1, 0.3, 0.5, 0.7, 0.9],
+           'distances_exponent': ['auto', 2, 5, 7],
+           'imbalance_ratio_threshold': ['auto', 0.5, 0.75, 1.0],
+           'k_neighbors': [3, 5]
+           }]
+       ),
     ],
     'classifiers': [
         ('CONSTANT CLASSIFIER', DummyClassifier(strategy='prior'), {}),
         ('LR', LogisticRegression(multi_class='multinomial', solver='sag', penalty='none', max_iter=1e4), {}),
         ('KNN', KNeighborsClassifier(), {'n_neighbors': [3, 5, 8]}),
-        ('RF', RandomForestClassifier(), {'max_depth': 
+        ('RF', RandomForestClassifier(), {'max_depth':
         [None, 3, 6], 'n_estimators': [50, 100, 200]})
     ],
     'scoring': ['accuracy', 'f1_macro', 'geometric_mean_score_macro'],
