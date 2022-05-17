8a4588afedfd717d6b141e3070556dbe31c3fecd
georgedouzas <gdouzas@icloud.com>
2022-05-17 15:18:56 +0300

    Removed incomplete projects

---
+++ projects/rl-trading/experiment/main.py
@@ -0,0 +1,16 @@
+"""
+Run the experiment.
+"""
+
+# Author: Georgios Douzas <gdouzas@icloud.com>
+# License: MIT
+
+
+def run_experiment():
+    """Run the experiment and log the experiment model."""
+    pass
+
+
+if __name__ == '__main__':
+
+    run_experiment()
